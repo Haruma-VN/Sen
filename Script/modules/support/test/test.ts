f9602a3812096047d73c1e0b2b7f16d5d53df9a7
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-07-02 13:10:05 +0700

    07.02.2023
    Debugging

--- Script/modules/support/test/test.ts
+++
@@ -1,7 +0,0 @@
-namespace Sen.Script.Modules.Support.Test {
-    export function Evaluate(): void {
-        Sen.Shell.Console.Print(null, "test");
-    }
-}
-
-Sen.Script.Modules.Support.Test.Evaluate();
