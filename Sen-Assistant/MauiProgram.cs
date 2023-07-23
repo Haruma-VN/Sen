b3ab2bd947db857beec20a56d88cdde0260b4928
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-07-23 21:05:09 +0700

    07.23.2023
    Update Interface

--- Sen-Assistant/MauiProgram.cs
+++ Sen-Assistant/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace Sen.Assistant
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fbUsv8C5eI.ttf", "PvZ2-Font");
                 });
 
 #if DEBUG
