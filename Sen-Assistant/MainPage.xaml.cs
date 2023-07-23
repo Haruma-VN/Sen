45f2b53fe2555f878dde0539ba1d22292b8c67a6
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-07-23 21:36:53 +0700

    07.23.2023
    Update

--- Sen-Assistant/MainPage.xaml.cs
+++ Sen-Assistant/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Sen.Assistant
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Sen.Assistant
 {
     public partial class MainPage : ContentPage
     {
@@ -6,7 +9,25 @@
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
 
+    }
+}
+
+public unsafe sealed class MainPageViewModel : INotifyPropertyChanged
+{
+    public unsafe event PropertyChangedEventHandler PropertyChanged;
+
+    public unsafe ICommand ExitCommand { get; }
+
+    public unsafe MainPageViewModel()
+    {
+        ExitCommand = new Command(OnExit);
+    }
+
+    private unsafe void OnExit()
+    {
+        Application.Current.Quit();
     }
 }
