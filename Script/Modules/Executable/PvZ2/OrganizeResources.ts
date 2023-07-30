0f789c01170a4efb551a66dddc97e1ac9067341b
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-07-30 16:41:32 +0700

    07.30.2023
    Added the support for organize Resources.json: https://github.com/Haruma-VN/Sen/issues/12

--- Script/Modules/Executable/PvZ2/OrganizeResources.ts
+++ Script/Modules/Executable/PvZ2/OrganizeResources.ts
@@ -23,11 +23,111 @@ namespace Sen.Script.Modules.Executable.PvZ2.OrganizeResources {
         forceOriginalVectorSymbolSize: bigint;
     }
 
+    /**
+     * Edit this to change order
+     */
+
+    export const Order: Sen.Script.Modules.Executable.PvZ2.OrganizeResources.OrganizeOrder = {
+        slot: 1n,
+        id: 2n,
+        path: 3n,
+        type: 4n,
+        atlas: 5n,
+        width: 6n,
+        height: 7n,
+        parent: 8n,
+        ah: 9n,
+        aw: 10n,
+        ax: 11n,
+        ay: 12n,
+        cols: 13n,
+        x: 14n,
+        y: 15n,
+        srcpath: 16n,
+        runtime: 17n,
+        forceOriginalVectorSymbolSize: 18n,
+    };
+
+    /**
+     * Structure
+     */
+
     export class OrganizeResources {
+        /**
+         * Organization
+         */
+
+        public organize_property: Array<string>;
+
+        /**
+         *
+         * @param resource - Provide resources.json
+         * @returns
+         */
+
         public constructor(private resource: Resources_Group_Structure_Template) {
+            this.organize_property = Object.keys(Sen.Script.Modules.Executable.PvZ2.OrganizeResources.Order).sort((a: string, b: string) =>
+                Number((Sen.Script.Modules.Executable.PvZ2.OrganizeResources.Order as any)[a] - (Sen.Script.Modules.Executable.PvZ2.OrganizeResources.Order as any)[b])
+            );
             return;
         }
 
-        // private Organize
+        /**
+         *
+         * @param res - Provide res
+         * @returns
+         */
+
+        private OrganizeResourceContent(res: Resource_Structure_Template): void {
+            res.resources.forEach((e, i) => {
+                const reorganizedObject: any = {};
+                for (const prop of this.organize_property) {
+                    if (e.hasOwnProperty(prop)) {
+                        reorganizedObject[prop] = (e as any)[prop];
+                    }
+                }
+                res.resources[i] = reorganizedObject;
+            });
+            Sen.Script.Modules.Third.JavaScript.FastSort.sort(res.resources).by([{ asc: (u) => u.id }]);
+            return;
+        }
+
+        /**
+         *
+         * @returns Do sort
+         */
+
+        public Work(): void {
+            this.resource.groups.forEach((res: any) => {
+                if (`resources` in res) {
+                    this.OrganizeResourceContent(res);
+                }
+            });
+            return;
+        }
+
+        /**
+         * Give resource back
+         */
+
+        public ExportResource(): Resources_Group_Structure_Template {
+            return this.resource;
+        }
+    }
+
+    export function Evaluate(): void {
+        Sen.Shell.Console.Print(
+            Sen.Script.Modules.Platform.Constraints.ConsoleColor.Cyan,
+            Sen.Script.Modules.System.Default.Localization.GetString("execution_argument").replace(/\{\}/g, Sen.Script.Modules.System.Default.Localization.GetString("input_resources_json_path"))
+        );
+        const file_in: string = Sen.Script.Modules.Interface.Arguments.InputPath("file");
+        const worker: Sen.Script.Modules.Executable.PvZ2.OrganizeResources.OrganizeResources = new Sen.Script.Modules.Executable.PvZ2.OrganizeResources.OrganizeResources(
+            Sen.Script.Modules.FileSystem.Json.ReadJson<Resources_Group_Structure_Template>(file_in)
+        );
+        worker.Work();
+        Sen.Script.Modules.FileSystem.Json.WriteJson<Resources_Group_Structure_Template>(file_in.replace(/((\.json))?$/i, ".organize.json"), worker.ExportResource(), true);
+        return;
     }
 }
+
+Sen.Script.Modules.Executable.PvZ2.OrganizeResources.Evaluate();
