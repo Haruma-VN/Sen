c4352e4899510c32f731ddf5c232ef044afd9101
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-08-14 03:18:46 +0700

    08.14.2023
    Remove

--- Internal/tool/encode/encode.hpp
+++ Internal/tool/encode/encode.hpp
@@ -1,12 +1,2 @@
 #pragma once
 #include "../../dependencies/etcpak/ProcessRGB.hpp"
-
-void Encode()
-{
-	int width = ...;
-	int height = ...;
-	unsigned char* imageData = ...;
-
-	// Compress image data using ETC1 format
-	unsigned char* compressedData = compress(imageData, width, height, ETC1);
-}
