8306facaa0a460f69175377d24dfa7e22c334d26
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-08-29 22:35:47 +0700

    08.29.2023
    Update build

--- Internal/build.sh
+++ Internal/build.sh
@@ -6,7 +6,7 @@ cd ./build
 if [ "$(uname)" = "Linux" ]; then
     cmake -DCMAKE_BUILD_TYPE=Release ..
 elif [ "$(uname)" = "Darwin" ]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/clang-16 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 ..
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang-16 -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ ..
 fi
 
 make
