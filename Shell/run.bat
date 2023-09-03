2ea174f3bacae94a25ef837c1251b19e2bfde796
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2023-09-03 12:04:22 +0700

    09.03.2023
    Update

---
+++ Shell/run.bat
@@ -0,0 +1,47 @@
+@echo off
+echo Start building
+dotnet publish >NUL
+
+
+
+set "makelink="
+set "startsen="
+set "rebuildscript="
+set mypath=%~dp0
+
+for %%x in (%*) do (
+	if %%x == l ( set /a makelink=y )
+	if %%x == s ( set /a startsen=y )
+	if %%x == b ( set /a rebuildscript=y )
+)
+
+if defined rebuildscript (
+	call :BUILD
+)
+
+if not exist bin\Release\net8.0\win-x64\publish\Scripts\ (
+	:BUILD
+	echo Building scripts
+	xcopy ..\Script bin\Release\net8.0\win-x64\publish\Script\ /e /h /y >NUL
+	cd bin\Release\net8.0\win-x64\publish\Script
+	call link.bat >NUL
+	cd %mypath:~0,-1%
+) 
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Sen.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%mypath:~0,-1%\bin\Release\net8.0\win-x64\publish\Sen.exe" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+if defined makelink (
+echo Creating Sen shortcut to desktop
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+)
+
+if defined startsen (
+echo Starting Sen
+start %mypath:~0,-1%\bin\Release\net8.0\win-x64\publish\Sen.exe
+)
